964f3398f69e77bfd556bd7f92989a2f24176640
Dplo1514 <dladlsgur3334@gmail.com>
2022-01-22 21:22:03 +0900

    220122

---
+++ ploSpring/src/main/java/plo/study/ploSpring/PloSpringApplication.java
@@ -0,0 +1,14 @@
+package plo.study.ploSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloSpringApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(PloSpringApplication.class, args);
+	}
+
+}
---
+++ ploSpring/src/test/java/plo/study/ploSpring/PloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package plo.study.ploSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

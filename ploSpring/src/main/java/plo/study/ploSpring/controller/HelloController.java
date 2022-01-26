f7bcf2475437b21dcff93ce536037923fb9cfdde
Dplo1514 <dladlsgur3334@gmail.com>
2022-01-26 22:49:59 +0900

    220126

--- ploSpring/src/main/java/plo/study/ploSpring/controller/HelloController.java
+++ ploSpring/src/main/java/plo/study/ploSpring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,4 +19,34 @@ public class HelloController {
         model.addAttribute("name" , name);
         return "hello-template";
     };
+
+    @GetMapping("hello-spring")
+    @ResponseBody // http통신 프로토콜 body부에 내용을 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody //ResponseBody : 객체를 입력받을시 default로 json타입의 데이터를 넘겨준다.
+                    //객체를 입력받을시 (기본 문자처리)viewresolver가 아닌 (기본 객체처리)MappingJackson2HttpMessageConverter가 동작
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+
+    static class Hello {
+        private String name;
+
+        //javabinjack , getterSetter , property방식
+        public String getName() { //getter
+            return name;
+        }
+
+        public void setName(String name) { //setter
+            this.name = name;
+        }
+    }
+
 }
